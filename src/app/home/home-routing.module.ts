8ee35e4a001ecc7c667ce4d545dfff387de9b717
eemiri <40573030+eemiri@users.noreply.github.com>
2020-10-27 11:22:24 +0100

    navigation zur map

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,6 +6,11 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
+    children:[
+      { path: 'auslieferer-map',
+        loadChildren: '/pages/auslieferungen/auslieferungen/auslieferer-map/auslieferer-map.module#AusliefererMapPageRoutingModule'
+      }
+    ]
   }
 ];
 
